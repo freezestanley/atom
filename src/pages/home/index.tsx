25bf55d56b0701e7050a2a2df9c1edbdfeeaf991
wangweiqi <me@wangweiqi.com>
2021-01-14 17:07:03 +0800

    i18n

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,128 +2,149 @@
  * @description 描述
  */
 import React, { FC } from 'react';
+import { useIntl } from 'umi';
 import styles from './styles/index.less';
 import logo from './images/logo-hksi.png';
 
 interface PropTypes { }
 const Home: FC<PropTypes> = function (props) {
+  const i18n = useIntl();
   return <main className={styles.home}>
     <section className={styles.banner}>
-      <h2>Enabling alternate avenue to Capital Markets:<br />Faster access to capital. Lower cost.</h2>
-      <h3>Powering end-to-end Security Token Offering solutions.</h3>
-      <a>GET IN TOUCH</a>
+      <h2>{i18n.formatMessage({ id: 'home_banner_head1' })}<br />{i18n.formatMessage({ id: 'home_banner_head2' })}</h2>
+      <h3>{i18n.formatMessage({ id: 'home_banner_text' })}</h3>
+      <a>{i18n.formatMessage({ id: 'home_getintouch' })}</a>
     </section>
     <section className={styles.highcosts}>
-      <h2>Existing captial raising processes = high costs</h2>
+      <h2>{i18n.formatMessage({ id: 'home_costs_head' })}</h2>
       <ul>
         <li>
-          <h3>Cumbersome<br />regulations</h3>
-          <p>Minimum profit threshold requirements and rigid shareholder structures</p>
+          <h3>
+            {i18n.formatMessage({ id: 'home_costs_item_head_1_1' })}
+            <br />
+            {i18n.formatMessage({ id: 'home_costs_item_head_1_2' })}
+          </h3>
+          <p>{i18n.formatMessage({ id: 'home_costs_item_text_1' })}</p>
         </li>
         <li>
-          <h3>Ownership<br />verification</h3>
-          <p>Difficult to trace back and reach ultimate beneficial holders</p>
+          <h3>
+            {i18n.formatMessage({ id: 'home_costs_item_head_2_1' })}
+            <br />
+            {i18n.formatMessage({ id: 'home_costs_item_head_2_2' })}
+          </h3>
+          <p>{i18n.formatMessage({ id: 'home_costs_item_text_2' })}</p>
         </li>
         <li>
-          <h3>Operational<br />inefficiencies</h3>
-          <p>Hefty fees paid to multiple intermediaries</p>
+          <h3>
+            {i18n.formatMessage({ id: 'home_costs_item_head_3_1' })}
+            <br />
+            {i18n.formatMessage({ id: 'home_costs_item_head_3_2' })}
+          </h3>
+          <p>{i18n.formatMessage({ id: 'home_costs_item_text_3' })}</p>
         </li>
         <li>
-          <h3>Lengthy<br />settlement</h3>
-          <p>Public securities (T+2), private investments (can extend for months)</p>
+          <h3>
+            {i18n.formatMessage({ id: 'home_costs_item_head_4_1' })}
+            <br />
+            {i18n.formatMessage({ id: 'home_costs_item_head_4_2' })}
+          </h3>
+          <p>{i18n.formatMessage({ id: 'home_costs_item_text_4' })}</p>
         </li>
       </ul>
     </section>
     <section className={styles.values}>
-      <h2>We DON’T displace! We build on the well-established capital market infrastructure in Hong Kong.</h2>
+      <h2>{i18n.formatMessage({ id: 'home_values_head' })}</h2>
       <div className={styles.valuesbox}>
-        <h3>A blockchain-powered platform that provides end-to-end STO solutions</h3>
-        <h3>Where we add value</h3>
+        <h3>{i18n.formatMessage({ id: 'home_values_chart_head' })}</h3>
+        <h3>{i18n.formatMessage({ id: 'home_values_chart_text' })}</h3>
         <ul>
           <li>
-            <h4>Issuers</h4>
-            <p>Equity</p>
-            <p>Debt</p>
-            <p>Real estate</p>
-            <p>Fine arts</p>
-            <p>etc.</p>
+            <h4>{i18n.formatMessage({ id: 'home_values_chart_item_head_1_1' })}</h4>
+            <p>{i18n.formatMessage({ id: 'home_values_chart_item_text_1' })}</p>
           </li>
           <li>
-            <h4>Product structuring</h4>
-            <p>Token Economics Advisory</p>
+            <h4>{i18n.formatMessage({ id: 'home_values_chart_item_head_2_1' })}</h4>
+            <p>{i18n.formatMessage({ id: 'home_values_chart_item_text_2' })}</p>
           </li>
           <li>
-            <h4>Token creation</h4>
+            <h4>{i18n.formatMessage({ id: 'home_values_chart_item_head_3_1' })}</h4>
           </li>
           <li>
-            <h4>Distribution</h4>
-            <p>Post-issuance Token Management</p>
+            <h4>{i18n.formatMessage({ id: 'home_values_chart_item_head_4_1' })}</h4>
+            <p>{i18n.formatMessage({ id: 'home_values_chart_item_text_4' })}</p>
           </li>
           <li>
-            <h4>Investors</h4>
+            <h4>{i18n.formatMessage({ id: 'home_values_chart_item_head_5_1' })}</h4>
           </li>
         </ul>
       </div>
     </section>
     <section className={styles.why}>
-      <h2>Why work with Atom 8?</h2>
+      <h2>{i18n.formatMessage({ id: 'home_why_head' })}</h2>
       <ul>
         <li>
-          <h3>Expertise</h3>
-          <p>A full dedicated team (across Hong Kong and Shanghai) specializing in STOs, with expertise spanning across capital markets and blockchain​</p>
-          <p>​We are ready to work with your issuing timeline!​</p>
+          <h3>{i18n.formatMessage({ id: 'home_why_item_head_1' })}</h3>
+          <p>{i18n.formatMessage({ id: 'home_why_item_text_1_1' })}</p>
+          <p>{i18n.formatMessage({ id: 'home_why_item_text_1_2' })}</p>
         </li>
         <li>
-          <h3>Future-proofed​</h3>
-          <p>We prepare a clear pathway for listing (SFC compliant) from the moment you start structuring security tokens​</p>
+          <h3>{i18n.formatMessage({ id: 'home_why_item_head_2' })}</h3>
+          <p>{i18n.formatMessage({ id: 'home_why_item_text_2_1' })}</p>
         </li>
         <li>
-          <h3>Security​</h3>
-          <p>Audited by Big Four​</p>
-          <p>Insurance coverage on security tokens</p><p>Capability in settling in both digital assets and fiat currencies locally in Hong Kong</p>
+          <h3>{i18n.formatMessage({ id: 'home_why_item_head_3' })}</h3>
+          <p>{i18n.formatMessage({ id: 'home_why_item_text_3_1' })}</p>
+          <p>{i18n.formatMessage({ id: 'home_why_item_text_3_2' })}</p>
+          <p>{i18n.formatMessage({ id: 'home_why_item_text_3_3' })}</p>
         </li>
         <li>
-          <h3>Investment<br />opportunity​</h3>
-          <p>A well funded early-stage digital asset company on the trajectory to future rounds of funding​</p>
+          <h3>{i18n.formatMessage({ id: 'home_why_item_head_4' })}</h3>
+          <p>{i18n.formatMessage({ id: 'home_why_item_text_4_1' })}</p>
         </li>
       </ul>
     </section>
     <section className={styles.advisory}>
-      <h2>Advisory &amp; Tech Providers</h2>
+      <h2>{i18n.formatMessage({ id: 'home_advisory_head' })}</h2>
       <div></div>
     </section>
     <section className={styles.products}>
-      <h2>Our Products / Services</h2>
-      <h3><a className={styles.products_highlight}>I want to issue security tokens</a><a>I want to invest in security tokens</a></h3>
-      <h4>You are well covered with our end-to-end solutions.</h4>
+      <h2>{i18n.formatMessage({ id: 'home_products_head' })}</h2>
+      <h3>
+        <a className={styles.products_highlight}>
+          {i18n.formatMessage({ id: 'home_products_choose_text_1' })}
+        </a>
+        <a>
+          {i18n.formatMessage({ id: 'home_products_choose_text_2' })}
+        </a>
+      </h3>
+      <h4>{i18n.formatMessage({ id: 'home_products_text' })}</h4>
       <ul>
         <li>
-          <h5>STO Advisory</h5>
-          <p>Token design</p>
-          <p>Security assurance, adhering to global SOC 2 testing principles (security, availability, processing integrity, confidentiality, privacy)</p>
-          <p>Payment and settlement logistics</p>
-          <p>Insurance coverage (protection against criminal activities and physical damage)</p>
-          <p>Corporate actions if any (mandatory and voluntary)</p>
-          <p>We are ready to work with your issuing timeline!​</p>
+          <h5>{i18n.formatMessage({ id: 'home_products_item_head_1' })}</h5>
+          <p>{i18n.formatMessage({ id: 'home_products_item_text_1_1' })}</p>
+          <p>{i18n.formatMessage({ id: 'home_products_item_text_1_2' })}</p>
+          <p>{i18n.formatMessage({ id: 'home_products_item_text_1_3' })}</p>
+          <p>{i18n.formatMessage({ id: 'home_products_item_text_1_4' })}</p>
+          <p>{i18n.formatMessage({ id: 'home_products_item_text_1_5' })}</p>
         </li>
         <li>
-          <h5>Security Token SaaS​</h5>
-          <p>Security token creation</p>
-          <p>Smart contract automation</p>
-          <p>System integration with external systems required (Document signing, reporting)</p>
-          <p>Security token stored at selected custodian(s)</p>
-          <p>Data protection and cybersecurity solutions</p>
-          <p>Token holder communication</p>
+          <h5>{i18n.formatMessage({ id: 'home_products_item_head_2' })}</h5>
+          <p>{i18n.formatMessage({ id: 'home_products_item_text_2_1' })}</p>
+          <p>{i18n.formatMessage({ id: 'home_products_item_text_2_2' })}</p>
+          <p>{i18n.formatMessage({ id: 'home_products_item_text_2_3' })}</p>
+          <p>{i18n.formatMessage({ id: 'home_products_item_text_2_4' })}</p>
+          <p>{i18n.formatMessage({ id: 'home_products_item_text_2_5' })}</p>
+          <p>{i18n.formatMessage({ id: 'home_products_item_text_2_5' })}</p>
         </li>
         <li>
-          <h5>Listing service</h5>
-          <p>Consultation on scope of and assistance on due diligence required</p>
-          <p>Drafting offer memorandum in connection to token listing on digital exchanges (i.e. minimize vetting time required)</p>
+          <h5>{i18n.formatMessage({ id: 'home_products_item_head_3' })}</h5>
+          <p>{i18n.formatMessage({ id: 'home_products_item_text_3_1' })}</p>
+          <p>{i18n.formatMessage({ id: 'home_products_item_text_3_2' })}</p>
         </li>
       </ul>
     </section>
     <section className={styles.partners}>
-      <h2>We work with industry-leading partners</h2>
+      <h2>{i18n.formatMessage({ id: 'home_partners_head' })}</h2>
       <div>
         <a>&lt;</a>
         <ul>
