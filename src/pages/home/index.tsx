b0252d487892bebf1f2c9a159dd04384c72b796a
wangweiqi <me@wangweiqi.com>
2021-01-15 13:00:20 +0800

    chang to flex layout

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -11,7 +11,8 @@ const Home: FC<PropTypes> = function (props) {
   const i18n = useIntl();
   return <main className={styles.home}>
     <section className={styles.banner}>
-      <h2>{i18n.formatMessage({ id: 'home_banner_head1' })}<br />{i18n.formatMessage({ id: 'home_banner_head2' })}</h2>
+      <h2>{i18n.formatMessage({ id: 'home_banner_head1' })}</h2>
+      <h2>{i18n.formatMessage({ id: 'home_banner_head2' })}</h2>
       <h3>{i18n.formatMessage({ id: 'home_banner_text' })}</h3>
       <a>{i18n.formatMessage({ id: 'home_getintouch' })}</a>
     </section>
@@ -54,12 +55,12 @@ const Home: FC<PropTypes> = function (props) {
     </section>
     <section className={styles.values}>
       <h2>{i18n.formatMessage({ id: 'home_values_head' })}</h2>
-      <div className={styles.valuesbox}>
+      <aside>
         <h3>{i18n.formatMessage({ id: 'home_values_chart_head' })}</h3>
         <h3>{i18n.formatMessage({ id: 'home_values_chart_text' })}</h3>
         <ul>
           <li>
-            <h4>{i18n.formatMessage({ id: 'home_values_chart_item_head_1' })}</h4>
+            <h3>{i18n.formatMessage({ id: 'home_values_chart_item_head_1' })}</h3>
             <p className={styles.list}>{i18n.formatMessage({ id: 'home_values_chart_item_text_1_1' })}</p>
             <p className={styles.list}>{i18n.formatMessage({ id: 'home_values_chart_item_text_1_2' })}</p>
             <p className={styles.list}>{i18n.formatMessage({ id: 'home_values_chart_item_text_1_3' })}</p>
@@ -67,25 +68,25 @@ const Home: FC<PropTypes> = function (props) {
             <p className={styles.list}>{i18n.formatMessage({ id: 'home_values_chart_item_text_1_5' })}</p>
           </li>
           <li>
-            <h4>{i18n.formatMessage({ id: 'home_values_chart_item_head_2' })}</h4>
+            <h3>{i18n.formatMessage({ id: 'home_values_chart_item_head_2' })}</h3>
             <p>{i18n.formatMessage({ id: 'home_values_chart_item_text_2' })}</p>
           </li>
           <li>
-            <h4>{i18n.formatMessage({ id: 'home_values_chart_item_head_3' })}</h4>
+            <h3>{i18n.formatMessage({ id: 'home_values_chart_item_head_3' })}</h3>
           </li>
           <li>
-            <h4>{i18n.formatMessage({ id: 'home_values_chart_item_head_4' })}</h4>
+            <h3>{i18n.formatMessage({ id: 'home_values_chart_item_head_4' })}</h3>
             <p>{i18n.formatMessage({ id: 'home_values_chart_item_text_4' })}</p>
           </li>
           <li>
-            <h4>{i18n.formatMessage({ id: 'home_values_chart_item_head_5' })}</h4>
+            <h3>{i18n.formatMessage({ id: 'home_values_chart_item_head_5' })}</h3>
           </li>
         </ul>
-        <aside>
+        <div>
           {i18n.formatMessage({ id: 'home_values_chart_comment_1' })}<br/>
           {i18n.formatMessage({ id: 'home_values_chart_comment_2' })}
-        </aside>
-      </div>
+        </div>
+      </aside>
     </section>
     <section className={styles.why}>
       <h2>{i18n.formatMessage({ id: 'home_why_head' })}</h2>
@@ -126,37 +127,37 @@ const Home: FC<PropTypes> = function (props) {
     </section>
     <section className={styles.products}>
       <h2>{i18n.formatMessage({ id: 'home_products_head' })}</h2>
-      <h3>
+      <div>
         <a className={styles.products_highlight}>
           {i18n.formatMessage({ id: 'home_products_choose_text_1' })}
         </a>
         <a>
           {i18n.formatMessage({ id: 'home_products_choose_text_2' })}
         </a>
-      </h3>
+      </div>
       <h4>{i18n.formatMessage({ id: 'home_products_text' })}</h4>
       <ul>
         <li>
           <h5>{i18n.formatMessage({ id: 'home_products_item_head_1' })}</h5>
-          <p>{i18n.formatMessage({ id: 'home_products_item_text_1_1' })}</p>
-          <p>{i18n.formatMessage({ id: 'home_products_item_text_1_2' })}</p>
-          <p>{i18n.formatMessage({ id: 'home_products_item_text_1_3' })}</p>
-          <p>{i18n.formatMessage({ id: 'home_products_item_text_1_4' })}</p>
-          <p>{i18n.formatMessage({ id: 'home_products_item_text_1_5' })}</p>
+          <p className={styles.list}>{i18n.formatMessage({ id: 'home_products_item_text_1_1' })}</p>
+          <p className={styles.list}>{i18n.formatMessage({ id: 'home_products_item_text_1_2' })}</p>
+          <p className={styles.list}>{i18n.formatMessage({ id: 'home_products_item_text_1_3' })}</p>
+          <p className={styles.list}>{i18n.formatMessage({ id: 'home_products_item_text_1_4' })}</p>
+          <p className={styles.list}>{i18n.formatMessage({ id: 'home_products_item_text_1_5' })}</p>
         </li>
         <li>
           <h5>{i18n.formatMessage({ id: 'home_products_item_head_2' })}</h5>
-          <p>{i18n.formatMessage({ id: 'home_products_item_text_2_1' })}</p>
-          <p>{i18n.formatMessage({ id: 'home_products_item_text_2_2' })}</p>
-          <p>{i18n.formatMessage({ id: 'home_products_item_text_2_3' })}</p>
-          <p>{i18n.formatMessage({ id: 'home_products_item_text_2_4' })}</p>
-          <p>{i18n.formatMessage({ id: 'home_products_item_text_2_5' })}</p>
-          <p>{i18n.formatMessage({ id: 'home_products_item_text_2_6' })}</p>
+          <p className={styles.list}>{i18n.formatMessage({ id: 'home_products_item_text_2_1' })}</p>
+          <p className={styles.list}>{i18n.formatMessage({ id: 'home_products_item_text_2_2' })}</p>
+          <p className={styles.list}>{i18n.formatMessage({ id: 'home_products_item_text_2_3' })}</p>
+          <p className={styles.list}>{i18n.formatMessage({ id: 'home_products_item_text_2_4' })}</p>
+          <p className={styles.list}>{i18n.formatMessage({ id: 'home_products_item_text_2_5' })}</p>
+          <p className={styles.list}>{i18n.formatMessage({ id: 'home_products_item_text_2_6' })}</p>
         </li>
         <li>
           <h5>{i18n.formatMessage({ id: 'home_products_item_head_3' })}</h5>
-          <p>{i18n.formatMessage({ id: 'home_products_item_text_3_1' })}</p>
-          <p>{i18n.formatMessage({ id: 'home_products_item_text_3_2' })}</p>
+          <p className={styles.list}>{i18n.formatMessage({ id: 'home_products_item_text_3_1' })}</p>
+          <p className={styles.list}>{i18n.formatMessage({ id: 'home_products_item_text_3_2' })}</p>
         </li>
       </ul>
     </section>
